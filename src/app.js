30a7597f211b814ad4dcc6d0607152d0e15091d9
Stephen Blythe <stephenblythe@gmail.com>
2019-08-26 15:50:10 +0100

    adds basic priority functionality

--- src/app.js
+++ src/app.js
@@ -4,14 +4,15 @@ document.addEventListener('DOMContentLoaded', () => {
 	new Vue({
 		el: "#app",
 		data: {
-			toDos: [{item: "Remember the milk", done: false},
-   		{item: "Learn Vue", done: true}],
-			newToDo: ""
+			toDos: [{item: "Remember the milk", done: false, priority: "low"},
+   		{item: "Learn Vue", done: true, priority: "high"}],
+			newToDo: "",
+			newToDoPriority: ""
 		},
 		methods: {
 			saveNewToDo: function() {
 				if (this.newToDo !== "") {
-					const toDoToAdd = {item: this.newToDo, done: false};
+					const toDoToAdd = {item: this.newToDo, done: false, priority: this.newToDoPriority};
 					this.toDos.push(toDoToAdd);
 					this.newToDo = "";
 				}
